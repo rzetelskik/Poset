0a8a8ab05cf28d0b99d00dbdb837b1210b2e0304
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2019-10-23 23:56:20 +0200

    implemented poset_insert

--- poset.cc
+++ poset.cc
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h>
 #include <string>
-#include <utility>
+#include <unordered_set>
+#include <unordered_map>
 
 using poset_id_t = unsigned long;
 using element_id_t = unsigned long long;
 using related_elements_t = std::unordered_set<element_id_t>;
 using dictionary_t = std::unordered_map<std::string, element_id_t>;
-using poset_t = std::pair<dictionary_t, std::unordered_map<element_id_t, std::pair<related_elements_t, related_elements_t>>>;
-using poset_map_t = std::map<poset_id_t, poset_t>;
+using poset_graph_t = std::unordered_map<element_id_t, std::pair<related_elements_t, related_elements_t>>;
+using poset_t = std::pair<dictionary_t, poset_graph_t>;
+using poset_map_t = std::unordered_map<poset_id_t, poset_t>;
 
 poset_map_t& poset_map() {
-    static poset_map_t ans;
-    return ans;
+    static poset_map_t poset_map;
+    return poset_map;
 }
 
 poset_id_t get_new_poset_id() {
@@ -22,28 +23,70 @@ poset_id_t get_new_poset_id() {
     return id;
 }
 
-unsigned long poset_new() {
-    poset_id_t id = get_new_poset_id();
-    poset_t poset;
-    poset_map_t().insert({id, poset});
+element_id_t get_new_element_id() {
+    static element_id_t id = 0;
+    id++;
     return id;
 }
 
-void poset_delete(unsigned long id) {
-    auto it = poset_map_t().find(id);
-    if (it != poset_map_t().end()){
-        poset_map_t().erase(it); // TODO kwestia usuniecia całego posetu z pamięci
-    }
+unsigned long poset_new() {
+    poset_id_t id = get_new_poset_id();
+    poset_t poset;
+    poset_map().insert({id, poset});
+    return id;
 }
 
-size_t poset_size(unsigned long id){
-    auto it = poset_map_t().find(id);
-    if (it != poset_map_t().end()){
+//void poset_delete(unsigned long id) {
+//    auto it = poset_map().find(id);
+//    if (it != poset_map().end()){
+//        poset_map().erase(it); // TODO kwestia usuniecia całego posetu z pamięci
+//    }
+//}
 
+//size_t poset_size(unsigned long id){
+//    auto it = poset_map().find(id);
+//    if (it != poset_map().end()){
+//
+//    }
+//}
+
+std::optional<element_id_t> get_element_id_from_dictionary(const dictionary_t& dictionary, const std::string& name) {
+    auto it = dictionary.find(name);
+    if (it != dictionary.end()) {
+        return it->second;
     }
+    return std::nullopt;
 }
 
-bool poset_insert(unsigned long id, char const *value);
+element_id_t insert_element_into_dictionary(dictionary_t& dictionary, const std::string& name) {
+    element_id_t id = get_new_element_id();
+    dictionary.insert({name, id});
+
+    return id;
+}
+
+void insert_element_into_poset_graph(poset_graph_t& poset_graph, element_id_t element_id) {
+    related_elements_t preceeding, proceeding;
+    poset_graph.insert({element_id, {preceeding, proceeding}});
+}
+
+bool poset_insert(unsigned long id, char const *value) {
+    auto it = poset_map().find(id);
+    if (it == poset_map().end()) {
+        return false;
+    }
+    poset_t& poset = it->second;
+
+    std::string element_name(value);
+    if (get_element_id_from_dictionary(poset.first, element_name).has_value()) {
+        return false;
+    }
+
+    element_id_t element_id = insert_element_into_dictionary(poset.first, element_name);
+    insert_element_into_poset_graph(poset.second, element_id);
+
+    return true;
+}
 
 bool poset_remove(unsigned long id, char const *value);
 
